2d17466884c89e10bf020e5e3105d1145d3d7a46
GustavoZambonSantos <gudssantos@hotmail.com>
2023-06-27 16:24:57 -0300

    BlogPessoal

--- PersonalBlog/src/App.tsx
+++ PersonalBlog/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './componentes/navbar/Navbar';
-import Footer from './componentes/footer/Footer';
+import Navbar from './componentes/estaticos/navbar/Navbar';
+import Footer from './componentes/estaticos/footer/Footer';
 import CadastroUsuario from './pages/cadastroUsuario/CadastroUsuario';
 import Home from './pages/home/home';
 import Login from './pages/login/Login';
@@ -12,46 +12,48 @@ import CadastroPost from './componentes/postagens/cadastroPost/CadastroPost';
 import CadastroTema from './componentes/temas/cadastroTema/CadastroTema';
 import DeletarPostagem from './componentes/postagens/deletarPostagem/DeletarPostagem';
 import DeletarTema from './componentes/temas/deletarTema/DeletarTema';
-
-
+import {Provider} from 'react-redux';
+import store from './store/store';
 
 function App() {
   return (
+    <Provider store={store}>
     <Router>
       <Navbar />
-    
-        <div style={{ minHeight: '100vh' }}>
+      <div style={{ minHeight: '100vh' }}>
         <Routes>
-        <Route path="/" element={<Login />} />
 
-        <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Login />} />
 
-        <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
 
-        <Route path="/cadastrousuario" element={<CadastroUsuario />} />
+          <Route path="/home" element={<Home />} />
 
-        <Route path="/temas" element={<ListaTema />} />
+          <Route path="/cadastrousuario" element={<CadastroUsuario />} />
 
-        <Route path="/posts" element={<ListaPostagem />} />
+          <Route path="/temas" element={<ListaTema />} />
 
-        <Route path="/formularioPostagem" element={<CadastroPost />} />
+          <Route path="/posts" element={<ListaPostagem />} />
 
-        <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
+          <Route path="/formularioPostagem" element={<CadastroPost />} />
 
-        <Route path="/formularioTema" element={<CadastroTema />} />
+          <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
 
-        <Route path="/formularioTema/:id" element={<CadastroTema />} />
+          <Route path="/formularioTema" element={<CadastroTema />} />
 
-        <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+          <Route path="/formularioTema/:id" element={<CadastroTema />} />
 
-        <Route path="/deletarTema/:id" element={<DeletarTema />} />
+          <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+
+          <Route path="/deletarTema/:id" element={<DeletarTema />} />
 
 
         </Routes>
-        </div>
-     
+      </div>
       <Footer />
+
     </Router>
+    </Provider>
   );
 }
 
--- PersonalBlog/src/services/service.tsx
+++ PersonalBlog/src/services/service.tsx
@@ -12,11 +12,8 @@ export const cadastroUsuario = async(url: any,dados: any,setDado: any) => {
     setDado(resposta.data)
 }
 
-
-/*Parametros*/
-export const login = async(url: any, dados:any, setDado: any) => {
-    const resposta = await api.post(url, dados)
-    /*retorno da aplicação*/
+export const login = async(url: any,dados: any,setDado: any) => { 
+    const resposta = await api.post(url,dados)
     setDado(resposta.data.token)
 }
 
