e25f87b6bb487aad7a9ee8b05380cccddbf437ab
GustavoZambonSantos <gudssantos@hotmail.com>
2023-06-28 08:09:24 -0300

    blog pessoal

--- PersonalBlog/src/componentes/estaticos/footer/Footer.tsx
+++ PersonalBlog/src/componentes/estaticos/footer/Footer.tsx
@@ -1,45 +1,55 @@
-import React from 'react';
-import InstagramIcon from '@material-ui/icons/Instagram';
+import { GitHub } from '@material-ui/icons';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import {Typography, Grid } from '@material-ui/core';
-import {Box} from '@mui/material';
+import { Typography, Grid, Box } from '@material-ui/core';
+import './footer.css'
+import { useSelector } from 'react-redux';
+import { TokenState } from '../../../store/tokens/tokenReducer';
 
-function Footer() {
+
+
+export default function Footer() {
+
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+    );
+
+    // eslint-disable-next-line no-var
+    var footerComponet;
+
+    if (token != '') {
+        footerComponent = <Grid container className='grid-footer'>
+            <Grid item xs={12}>
+                <Box className='box1-footer'>
+                    <Typography variant="h5" className='textos'>Visite minhas redes sociais</Typography>
+                    <Box >
+                        <a href="https://www.facebook.com/generationbrasil" target="_blank">
+                            <FacebookIcon className='network-footer' />
+                        </a>
+                        <a href="https://github.com/FelipeSdsilva" target="_blank">
+                            <GitHub className='network-footer' />
+                        </a>
+                        <a href="https://www.linkedin.com/in/felipesdsilva/" target="_blank">
+                            <LinkedInIcon className='network-footer' />
+                        </a>
+                    </Box>
+                </Box>
+                <Box className='box2-footer'>
+                    <Box paddingTop={1}>
+                        <Typography variant="subtitle2" gutterBottom className='textos' >© 2023 Copyright:</Typography>
+                    </Box>
+                    <Box>
+                        <Typography variant="subtitle2" gutterBottom className='textos' >Felipe Sousa</Typography>
+                    </Box>
+                </Box>
+            </Grid>
+        </Grid>
+    }
+
+
+
     return (
         <>
-            <Grid container direction="row" justifyContent="center" alignItems="center">
-                <Grid alignItems="center" item xs={12}>
-                    <Box style={{ backgroundColor: "#3F51B5", height: "120px" }}>
-                        <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography>
-                        </Box>
-                        <Box display="flex" alignItems="center" justifyContent="center">
-                            <a href="https://www.facebook.com/generationbrasil" target="_blank" rel="noopener noreferrer">
-                                <FacebookIcon style={{ fontSize: 60, color: "white" }} />
-                            </a>
-                            <a href="https://www.instagram.com/generationbrasil/" target="_blank" rel="noopener noreferrer">
-                                <InstagramIcon style={{ fontSize: 60, color: "white" }} />
-                            </a>
-                            <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank" rel="noopener noreferrer">
-                                <LinkedInIcon style={{ fontSize: 60, color: "white" }} />
-                            </a>
-                        </Box>
-                    </Box>
-                    <Box style={{ backgroundColor: "#303F9F", height: "60px" }}>
-                        <Box paddingTop={1}>
-                            <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >© 2020 Copyright:</Typography>
-                        </Box>
-                        <Box>
-                            <a target="_blank" href="https://brasil.generation.org" rel="noopener noreferrer">
-                                <Typography variant="subtitle2" gutterBottom style={{ color: "white" }} align="center">brasil.generation.org</Typography>
-                            </a>
-                        </Box>
-                    </Box>
-                </Grid>
-            </Grid>
-        </>
-    )
+            {footerComponent}
+        </>)
 }
-
-export default Footer;
